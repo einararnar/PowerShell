2a5701df468dd474e25ffb6d06bcbcfeef08a213
einararnar <einararnarm@hotmail.com>
2023-06-02 19:41:50 +0000

    Empty form script

---
+++ PS GUI/New blank form.ps1
@@ -0,0 +1,12 @@
+﻿# Load .NET framework classes
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+# Create a new blank form/window
+$main = New-Object System.Windows.Forms.Form
+$main.Text = 'New Form'
+$main.Size = New-Object System.Drawing.Size(300,200)
+$main.StartPosition = 'CenterScreen'
+
+$main.TopMost = $true
+$main.ShowDialog()
